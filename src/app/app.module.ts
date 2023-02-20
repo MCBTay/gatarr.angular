70fc5568b2e0024b0bdc86cecc40e0f8fed84042
Bryan Taylor <mcbtay@gmail.com>
2023-02-19 22:50:53 -0500

    simple tab interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,27 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SonarrService } from './sonarr.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+import { SonarrModule } from "./sonarr/sonarr.module";
+import { RadarrModule } from './radarr/radarr.module';
+import { SettingsModule } from './settings/settings.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [	
+        AppComponent
+   ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatTabsModule,
+        SonarrModule,
+        RadarrModule,
+        SettingsModule
+    ]
 })
 export class AppModule { }
