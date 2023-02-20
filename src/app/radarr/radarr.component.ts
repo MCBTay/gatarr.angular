b9caa6717e06b02afdc6571a234bd0add62d6e19
Bryan Taylor <mcbtay@gmail.com>
2023-02-19 22:51:08 -0500

    adding radarr and settings components

---
+++ src/app/radarr/radarr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-radarr',
+  templateUrl: './radarr.component.html',
+  styleUrls: ['./radarr.component.css']
+})
+export class RadarrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/radarr/radarr.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RadarrComponent } from './radarr.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [RadarrComponent],
+  declarations: [RadarrComponent]
+})
+export class RadarrModule { }
