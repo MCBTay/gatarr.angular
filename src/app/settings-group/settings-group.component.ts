b68cadae271e970efc820602ccb5010774708364
Bryan Taylor <mcbtay@gmail.com>
2023-02-28 19:47:13 -0500

    save/load settings

--- src/app/settings-group/settings-group.component.ts
+++ src/app/settings-group/settings-group.component.ts
@@ -6,14 +6,13 @@ import { ArrSettings } from './arr-settings';
   selector: 'app-settings-group',
   templateUrl: './settings-group.component.html',
   styleUrls: ['./settings-group.component.css'],
-  inputs: ['settings']
+  inputs: ['name']
 })
 export class SettingsGroupComponent implements OnInit {
   public settingsForm : FormGroup;
+  public name : string = "";
 
-  constructor(
-    private formBuilder : FormBuilder,
-    public settings : ArrSettings) { 
+  constructor(private formBuilder : FormBuilder) { 
     this.settingsForm = this.formBuilder.group({
       host: ['', [ Validators.required, Validators.pattern("(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)") ]],
       port: ['', [ Validators.required, Validators.pattern("^[0-9]*$") ]],
@@ -23,10 +22,20 @@ export class SettingsGroupComponent implements OnInit {
   }
 
   ngOnInit() {
+    var localStorageSettings = localStorage.getItem(this.name);
+    if (localStorageSettings == null) return;
+
+    let settings : ArrSettings = JSON.parse(localStorageSettings);
+
+    this.settingsForm.setValue([ "host", settings.host ]);
+    this.settingsForm.setValue([ "port", settings.port ]);
+    this.settingsForm.setValue([ "isSecure", settings.isSecure ]);
+    this.settingsForm.setValue([ "apiKey", settings.apiKey ]);
   }
 
   public onSubmit() {
-    console.log("saving!");
-    console.log(this.settingsForm.value);
+    if (this.settingsForm.pristine) return;
+
+    localStorage.setItem(this.name, JSON.stringify(this.settingsForm.value));
   }
 }
