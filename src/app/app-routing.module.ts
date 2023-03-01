52b05c78f895735e08f6f5450e83917a6de98b38
Bryan Taylor <mcbtay@gmail.com>
2023-02-28 19:44:40 -0500

    setting up initial routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RadarrComponent } from './radarr/radarr.component';
+import { SettingsComponent } from './settings/settings.component';
+import { SonarrComponent } from './sonarr/sonarr.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sonarr', component: SonarrComponent },
+  { path: 'radarr', component: RadarrComponent },
+  { path: 'settings', component: SettingsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
